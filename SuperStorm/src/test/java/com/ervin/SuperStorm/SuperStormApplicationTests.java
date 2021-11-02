b5aac1124ef07d78fd45394f690df9491e4f7ebe
zhang.yizhe@northeastern.edu <zhang.yizhe@northeastern.edu>
2021-11-02 22:48:23 +0800

    move out tmpl_springboot to backend_spring and move in superstorm

---
+++ SuperStorm/src/test/java/com/ervin/SuperStorm/SuperStormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ervin.SuperStorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperStormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
